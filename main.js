31be4eaa130abeb8ef9f4b394d0c39b1131992da
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-02 14:03:00 +0530

    Preview Fixes

--- main.js
+++ main.js
@@ -5,22 +5,4 @@ import { setupCounter } from './counter.js'
 import './index.scss';
 import './index.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+document.querySelector('#root').innerHTML = `<h1> Vite </h1>`;
