7c54db97e667b98ee570acbc5f8a9f0a125f7c4a
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-02 15:56:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,12 +13,13 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>Stay Tuned For Future Updates !</h1><br>
+    <h1><em>Wanna create stunning websites? { <code>Learn to Code</code> }</em></h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite logo to learn more
+      You know what is this all. Right?
     </p>
   </div>
 `
