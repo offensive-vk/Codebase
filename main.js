6afef3b0621956e28b240c0332e44f58cfe0d85b
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-18 17:34:28 +0530

    Finales

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ import './assets/index.js'
 import './styles/index.css'
 import './styles/wrapper.css';
 
-import { loadSectionSecond, loadWrapper } from './assets/index.js';
+import { loadWrapper } from './assets/index.js';
 import { loadSectionFirst } from './assets/index.js';
 
 ( async() => {
     await loadWrapper('Codebase.dev ', 'Helping you with Programming.');
     await loadSectionFirst();
-    // await loadSectionSecond();
 })();
